8c774e24938fa341f4b56addb9745eae8d12ec01
dlrandy <qq1208484996@qq.com>
2023-01-09 20:58:48 +0800

    fix:build assets error

--- src/client/sound/soundList.ts
+++ src/client/sound/soundList.ts
@@ -1,7 +1,7 @@
 const soundList = {
-    'coin':'/soundBank/coin.mp3',
-    'environment':'/soundBank/retreat.mp3',
-    'win':'/soundBank/win.mp3',
-    'bep':'/soundBank/sxf.mp3'
+    'coin':'soundBank/coin.mp3',
+    'environment':'soundBank/retreat.mp3',
+    'win':'soundBank/win.mp3',
+    'bep':'soundBank/sxf.mp3'
 }
 export default soundList
